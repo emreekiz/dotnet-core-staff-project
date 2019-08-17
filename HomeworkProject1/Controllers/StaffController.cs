2da13c7d34c4ed9a2464ae24c0e5650d7b31bf84
Emre-PC <Emre-PC@EMRE-PC>
2019-08-17 21:03:24 +0300

    Commit Staged

---
+++ HomeworkProject1/Controllers/StaffController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeworkProject1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkProject1.Controllers
+{
+    public class StaffController : Controller
+    {
+        
+        public static List<Staff> staffs;
+        public IActionResult Index()
+        {
+            List<Staff> staffList = Staff.GetSampleData();
+            if (staffs != null && staffs.Any())
+            {
+                staffList.AddRange(staffs);
+            }
+            return View(staffList);
+          
+        }
+
+        public IActionResult Detail(int id)
+        {
+            List<Staff> staffList = Staff.GetSampleData();
+            if (staffs != null && staffs.Any())
+            {
+                staffList.AddRange(staffs);
+            }
+            Staff item = staffList.Where(x => x.Id == id).FirstOrDefault();
+            return View(item);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Staff modelStaff)
+        {
+            if (ModelState.IsValid)
+            {
+                if (staffs==null)
+                {
+                    staffs=new List<Staff>();
+                }
+                staffs.Add(modelStaff);
+                return RedirectToAction("Detail", new {id = modelStaff.Id});
+            }
+
+            return View(modelStaff);
+
+
+
+
+        }
+    }
+}
---
+++ HomeworkProject1/Models/Staff.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HomeworkProject1.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string Title  { get; set; }
+        public string Address { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime BirthDate { get; set; }
+        [Required]
+        public string Image { get; set; }
+        public static List<Staff> GetSampleData()
+        {
+            var person1 = new Staff
+            {
+                Id = 1,
+                Name = "Emre",
+                Surname = "Ekiz",
+                Department = "Bilgi İşlem",
+                Title = "Bilgisayar Mühendisi",
+                Address = "1530 County Rd #155, Post, TX, 79356",
+                StartDate = Convert.ToDateTime("01.01.2015"),
+                BirthDate = Convert.ToDateTime("26.08.1995"),
+                Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxKlQbecwqKkurzvZ20x6Ta0WCpD97Z1zsUfLZF7HGx-gfd1Mc"
+               
+
+
+            };
+            var person2 = new Staff
+            {
+                Id = 2,
+                Name = "Erdem",
+                Surname = "Ekiz",
+                Department = "Pazarlama",
+                Title = "Satış Sorumlusu",
+                Address = "742 Aspen Cir, Red Hill, PA, 18076",
+                StartDate = Convert.ToDateTime("23.05.2017"),
+                BirthDate = Convert.ToDateTime("11.03.1992"),
+                Image = "https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-542759665.jpg"
+
+
+            };
+            var person3 = new Staff
+            {
+                Id = 3,
+                Name = "Ömer",
+                Surname = "Ekiz",
+                Department = "Üretim",
+                Title = "Üretim Müdürü",
+                Address = "742 Aspen Cir, Red Hill, PA, 18076",
+                StartDate = Convert.ToDateTime("11.02.2015"),
+                BirthDate = Convert.ToDateTime("08.01.1963"),
+                Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS59TrTGVMf7FVScv2aopiEKDWa0U6EMQXiCvQ3b-rr9xuul6ov"
+
+
+            };
+            List<Staff> sampleData=new List<Staff>();
+            sampleData.Add(person1);
+            sampleData.Add(person2);
+            sampleData.Add(person3);
+
+            return sampleData;
+        }
+
+    }
+
+   
+}
